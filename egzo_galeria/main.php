08aed78d178f10f82f4809f212c7eb98680e4b7c
piterec <73880934+piterec@users.noreply.github.com>
2025-11-29 17:11:26 +0100

    Add files via upload

---
+++ egzo_galeria/main.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+
+<html lang="en">
+<head>
+    <title>Interactive gallery of exoskeletons</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+    <script src="script.js">
+</body>
+</html>
